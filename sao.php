9bfc48dcfcb06516751b89a2e48fe27daa009cd5
brian <koreanderson@gmail.com>
2014-09-12 15:19:18 -0400

    scss integration

---
+++ sao.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>SAO Interface Test</title>
+	<link rel="stylesheet" href="assets/css/sao.css">
+</head>
+<body>
+	<div class="css">
+		<a id="trigger" href="#">Button</a>
+		<ul id="menu">
+			<li class="item">
+				<a href="#">1</a>
+			</li>
+			<li class="item">
+				<a href="#">2</a>
+			</li>
+			<li class="item">
+				<a href="#">3</a>
+			</li>
+			<!--<li class="parent item">-->
+				<!--Sub-->
+				<!--<ul class="sub-menu">-->
+					<!--<li class="item">1</li>-->
+					<!--<li class="item">2</li>-->
+					<!--<li class="item">3</li>-->
+				<!--</ul>-->
+			<!--</li>-->
+		</ul>
+	</div><!--/css-->
+	<div class="jquery">
+
+	</div>
+</body>
+</html>
